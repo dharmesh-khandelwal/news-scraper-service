683d5e836a23825cc4ffee66f10e84e44ee226c8
Dharmesh Khandelwal <Dharmesh.Khandelwal@mindtree.com>
2018-12-29 00:56:03 +0530

    Bootstrapped project with spring initializr

---
+++ src/main/java/com/handzap/newsscraperservice/NewsScraperServiceApplication.java
@@ -0,0 +1,14 @@
+package com.handzap.newsscraperservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsScraperServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsScraperServiceApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/handzap/newsscraperservice/NewsScraperServiceApplicationTests.java
@@ -0,0 +1,17 @@
+package com.handzap.newsscraperservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NewsScraperServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
