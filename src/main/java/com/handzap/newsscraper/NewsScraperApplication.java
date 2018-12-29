80142e54f94c831c3c55bbdaa8f020fa2a7178d2
Dharmesh Khandelwal <Dharmesh.Khandelwal@mindtree.com>
2018-12-29 19:49:15 +0530

    Updated doc

--- src/main/java/com/handzap/newsscraper/NewsScraperApplication.java
+++ src/main/java/com/handzap/newsscraper/NewsScraperApplication.java
@@ -3,11 +3,12 @@ package com.handzap.newsscraper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.handzap.newsscraper.init.NewsScraperAsyncTask;
 import com.handzap.newsscraper.init.NewsScraperInitializer;
 
 /**
  * News Scraper Service spring boot application. Scraping process begins in
- * {@link NewsScraperInitializer}
+ * {@link NewsScraperInitializer} with help of {@link NewsScraperAsyncTask}
  * 
  * @author Dharmesh Khandelwal
  * @since 1.0.0
